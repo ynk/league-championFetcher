a147662bfc477d2e9a0e6dd0661b719d8a1c247e
p1r_a_t3 <10481417+ibtehaz-shawon@users.noreply.github.com>
2019-07-15 10:18:59 +0600

    Flask added, Code reformatted

--- championFetcher.py
+++ championFetcher.py
@@ -3,21 +3,24 @@
 try:
     import requests
 except ImportError:
-    print("\"requests\" module is not installed. \nPlease look at the Github instructions. -> https://github.com/YannickDC/league-championFetcher")
+    print("\"requests\" module is not installed. \nPlease look at the Github instructions. -> "
+          "https://github.com/YannickDC/league-championFetcher")
     quit(1)
+
 import json
 from collections import Counter
 import sys
 
-api_key = "" # Enter your own API key in here.
+api_key = ""  # Enter your own API key in here.
 if not api_key:
     if len(sys.argv) == 2:
         api_key = sys.argv[1]
     else:
-        print("You forgot to change the api key.\nPlease look at the Github instructions. -> https://github.com/YannickDC/league-championFetcher")
+        print("You forgot to change the api key.\nPlease look at the Github instructions. -> "
+              "https://github.com/YannickDC/league-championFetcher")
         quit(1)
 
-valid_servers = ['euw1', 'na1', 'eun1', 'br1', 'la1', 'la2', 'tr1', 'jp1', 'kr','ru','oc1']
+valid_servers = ['euw1', 'na1', 'eun1', 'br1', 'la1', 'la2', 'tr1', 'jp1', 'kr', 'ru', 'oc1']
 
 account_name = None
 server = None
@@ -33,7 +36,8 @@ while not server in valid_servers:
         print("Invalid input.")
 
 print('Trying to find summoner...')
-account_data = requests.get("https://{}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{}?api_key={}".format(server, account_name, api_key))
+account_data = requests.get("https://{}.api.riotgames.com/lol/summoner/v4/summoners/by-name"
+                            "/{}?api_key={}".format(server, account_name, api_key))
 if account_data.status_code == 404:
     print("Summoner \"{}\" does not exist!".format(account_name))
     quit(1)
@@ -41,18 +45,22 @@ elif account_data.status_code == 403:
     print("Permission denied when requesting account data for summoner \"{}\"".format(account_name))
     quit(1)
 elif account_data.status_code != 200:
-    print("Recieved error HTTP {} when requesting data for summoner \"{}\"".format(account_data.status_code, account_name))
+    print("Recieved error HTTP {} when requesting data for summoner \"{}\"".format(account_data.status_code,
+                                                                                   account_name))
     quit(1)
 
 account_id = json.loads(account_data.text)["accountId"]
 print("Summoner has been found. Fetching match data. This might take a while...")
 
-matchlist_data = requests.get("https://{}.api.riotgames.com/lol/match/v4/matchlists/by-account/{}?beginIndex=9999999&api_key={}".format(server, account_id, api_key)) # little trick to get that.
+matchlist_data = requests.get("https://{}.api.riotgames.com/lol/match/v4/matchlists"
+                              "/by-account/{}?beginIndex=9999999&api_key={}".format(server, account_id,
+                                                                                    api_key))  # little trick to get that.
 if matchlist_data.status_code == 403:
     print("Permission denied when requesting total number of matches for summoner \"{}\"".format(account_name))
     quit(1)
 elif matchlist_data.status_code != 200:
-    print("Recieved error HTTP {} when requesting total number of matches for summoner \"{}\"".format(matchlist_data.status_code, account_name))
+    print("Received error HTTP {} when requesting total number of matches"
+          " for summoner \"{}\"".format(matchlist_data.status_code, account_name))
     quit(1)
 
 total_games = json.loads(matchlist_data.text)["totalGames"]
@@ -203,29 +211,31 @@ champion_names = {
     517: 'Sylas',
     518: 'Neeko',
     555: 'Pyke',
-    }
+}
 
 champions = []
 
-
-limit = int(total_games / 100 ) + 1 #always wanna do more then actually needed to prevent that matches are missing
+# always wanna do more then actually needed to prevent that matches are missing
+limit = int(total_games / 100) + 1
 index_end = 100
 index_start = 0
 
 for a in range(limit):
-    matches_data = requests.get("https://{}.api.riotgames.com/lol/match/v4/matchlists/by-account/{}?endIndex={}&beginIndex={}&api_key={}".format(server, account_id, index_end, index_start, api_key))
+    matches_data = requests.get("https://{}.api.riotgames.com/lol/match/v4/matchlists"
+                                "/by-account/{}?endIndex={}&beginIndex={}&api_key={}"
+                                .format(server, account_id, index_end, index_start, api_key))
     if matches_data.status_code == 200:
         matches = json.loads(matches_data.text)['matches']
         for match in matches:
             champions.append(champion_names[match['champion']])
     else:
-        print("Recieved error HTTP {} when requesting match info for summoner \"{}\"".format(matches_data.status_code, account_name))
+        print("Received error HTTP {} when requesting match info for "
+              "summoner \"{}\"".format(matches_data.status_code, account_name))
         print(matches_data)
         quit(1)
 
-
     index_start = index_end
-    index_end +=100
+    index_end += 100
 
 counterOutput = Counter(champions)
 print("[ Results for {} ] ".format(account_name))
@@ -233,6 +243,6 @@ print("Total games on this account: ", total_games)
 print("Total champions in this result set: {}".format(len(counterOutput)))
 print("[  Champion Results  ]")
 print("")
-for key,value in counterOutput.most_common():
-    print(key, value)
 
+for key, value in counterOutput.most_common():
+    print(key, value)
